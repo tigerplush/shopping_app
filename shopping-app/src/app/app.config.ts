c9833ab2dfd165c2083b03259b9f20f887e40e5f
tigerplush <christopher.putz@corgibase.de>
2025-02-21 15:19:27 +0100

    fixed big oopsie

--- shopping-app/src/app/app.config.ts
+++ shopping-app/src/app/app.config.ts
@@ -5,7 +5,21 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "shopping-app-63b25", appId: "1:85523711381:web:8802c849f53991cd8ee29c", storageBucket: "shopping-app-63b25.firebasestorage.app", apiKey: "AIzaSyBUg0NLjI9uW4AmS7wvdXQeXkEZtPTVwN4", authDomain: "shopping-app-63b25.firebaseapp.com", messagingSenderId: "85523711381" })), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true })
+    , provideRouter(routes)
+    , provideFirebaseApp(() => initializeApp({
+      projectId: environment.projectId
+      , appId: environment.appId
+      , storageBucket: environment.storageBucket
+      , apiKey: environment.apiKey
+      , authDomain: environment.authDomain
+      , messagingSenderId: environment.messagingSenderId
+    }))
+    , provideFirestore(() => getFirestore())
+    , provideAnimationsAsync()
+  ]
 };
